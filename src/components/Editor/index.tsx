57c05472869c746b6dadf39f0f625f8fcf92fca1
jacobcoro <xiaokourou@gmail.com>
2022-10-10 00:21:33 +0800

    add debouncer

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,9 +1,9 @@
 import React, { useCallback, useContext } from 'react';
-
+import { debounce } from 'lodash';
 import styles from './Editor.module.scss';
 import { NotesContext } from 'components/notes/NotesContext';
 import Editor from './MilkdownEditor';
-
+const debounceTime = 2000;
 export type OnEditorChange = (markdown: string) => void;
 
 const MarkDownEditor: React.FC<{
@@ -11,22 +11,38 @@ const MarkDownEditor: React.FC<{
   noteId: string;
 }> = ({ noteId, readOnly }) => {
   const { notes, updateNote } = useContext(NotesContext);
+  // TODO: check if the editor changes when remote changes.
 
-  // will need a local value for the note, then set locally on change, then debounce update db, but also change local on detect db version change
+  if (!notes) return <div>Loading note...</div>;
+
+  const [noteText, setNoteText] = React.useState(notes[noteId]?.text ?? '#');
+
+  const debouncedUpdate = React.useRef(
+    debounce((text: string) => {
+      updateNote(text, noteId);
+    }, debounceTime)
+  ).current;
+
+  // unregister debouncer if it hasn't been called yet
+  React.useEffect(() => {
+    return () => {
+      debouncedUpdate.cancel();
+    };
+  }, [debouncedUpdate]);
 
   const handleUpdate = useCallback(
     (text: string) => {
-      // gets rid of a bug where empty text prevent the preview from showing. Also starts each blank note with a heading
-      updateNote(text === '' ? '#' : text, noteId);
-    },
-    [noteId, updateNote]
-  );
-  if (!notes) return <div>Loading note...</div>;
+      setNoteText(text === '' ? '#' : text);
 
-  const text = notes[noteId]?.text ? notes[noteId].text : '#';
+      // gets rid of a bug where empty text prevent the preview from showing. Also starts each blank note with a heading
+      debouncedUpdate(text === '' ? '#' : text);
+    },
+    [updateNote]
+  );
+
   return (
     <div className={styles.root}>
-      <Editor onChange={handleUpdate} content={text} readOnly={readOnly} />
+      <Editor onChange={handleUpdate} content={noteText} readOnly={readOnly} />
     </div>
   );
 };
